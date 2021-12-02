ea720ed06a77b6b7186cbfcdfccbf1d606e4dd3b
yalin-yoshva <65439755+yalin-yoshva@users.noreply.github.com>
2021-12-02 14:04:46 +0530

    Update Project_main.c

--- 3_Implementation/Project_main.c
+++ 3_Implementation/Project_main.c
@@ -1,1 +1,21 @@
+#include <avr/io.h>
+int main(void)
+{
+  DDRB=0x00;
+  PORTB=0xFF;
+  DDRC=0xFF;
+  while(1)
+  {
+if(PINB&0b00000001)//02
+{
+PORTC=0x00;   
+}
+else
+{ 
+PORTC=0x01; 
+}
+  }
+  return 0;
+}
+
 
